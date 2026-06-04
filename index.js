cf878b031550799b747ae1b30bb2d5a6d54db7b4
aiacon6666 <aiacon6666@users.noreply.github.com>
2026-06-04 11:48:21 +0530

    Remove performance import and bump version

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
-import { measureStartup } from './src/utils/performance';
 
 // Log performance in development only
 if (__DEV__) {
-  measureStartup();
 }
 
 registerRootComponent(App);
